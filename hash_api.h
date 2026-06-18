9614db663b870d9b08581d884acb75541146bd2f
rimopa <rimopapomir@gmail.com>
2026-06-18 12:53:35 -0300

    256 not safe

--- hash_api.h
+++ hash_api.h
@@ -1,11 +1,3 @@
-/*********************************************************************
-* Filename:   hash_api.h
-* Author:     rimopa (rimopapomir AT gmail.com)
-* Disclaimer: This code is presented "as is" without any guarantees.
-* Details:    Header file for communicating information
-              related to hash functions confidently and efficiently.
-*********************************************************************/
-
 #ifndef HASH_DISTRIB_HASH_API_TYPE
 #define HASH_DISTRIB_HASH_API_TYPE
 
@@ -15,7 +7,7 @@ typedef struct
 {
     const char *name; // Name of your hash algorithm
     size_t ctx_size;  // Size you need for your context in bytes
-    size_t out_size;  // Size of your returned key in bytes. Must be >= 1 and <= 256
+    size_t out_size;  // Size of your returned key in bytes. Must be >= 1 and <= 255
     void (*init)(void *ctx);
     void (*update)(void *ctx, const unsigned char *data, size_t len);
     void (*final)(void *ctx, unsigned char *out);
