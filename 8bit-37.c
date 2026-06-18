b8822449a695cb497f835fd5fc78e5b5c7c34210
rimopa <rimopapomir@gmail.com>
2026-06-18 00:04:49 -0300

    example hash 37

---
+++ 8bit-37.c
@@ -0,0 +1,30 @@
+#include "hash_api.h"
+#include <stdint.h>
+#include <memory.h>
+
+void init(void *ctx)
+{
+}
+void update(void *ctx, const unsigned char *data, size_t len)
+{
+}
+void final(void *ctx, unsigned char *out)
+{
+    uint8_t thirty_seven = 37;
+    memcpy(out, &thirty_seven, sizeof(uint8_t));
+}
+
+static const HashAPI api = {
+    .name = "I like 37",
+
+    .ctx_size = 0,
+    .out_size = sizeof(uint8_t),
+
+    .init = init,
+    .update = update,
+    .final = final};
+
+HashAPI hash_api(void)
+{
+    return api;
+}
